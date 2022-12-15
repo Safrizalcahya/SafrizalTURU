5d1e37fc5eb0ff9e966ede99e4c68b5f7bef439b
safrizalcahya <mauluddiva25@gmail.com>
2022-12-15 17:48:49 +0700

    kedua

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://safrizallast.web.app" // TODO: replace this
 const studentName = 'Safrizal Cahya Nugraha' // TODO: replace this
 const studentId = 'FE3232849' // TODO: replace this
 
